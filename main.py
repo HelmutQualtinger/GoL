8cbee1ddc7a87d39a78c09b9df9ad3ced68c5853
Papa <qualcunodue@gmail.com>
2021-11-17 16:12:04 +0100

    added file writing and readin in menu

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import sys
+import tkinter
+from tkinter import filedialog as fd
 
 sys.path.append(".")
 from GoLOO import GoL
-import tkinter
 
 gol = GoL()
-
 colors = ["#FFFFFF", "#FFFF80", "#FFFF00", "#FF8000", "#FF0020", "#800040", "#400060", "#000040", "#200010", "#000000"]
-
 
 def updateCanvas(C):
     global rect
     global refreshLabel
     global refreshScreen
     global CellCounterLabel
+    global gol
     gol.countNeighbours()
     refreshLabel.configure(text="Gernerations: " + str(refreshScreen))
     CellCounterLabel.configure(text="Living Cells:  " + str(len(gol.universe)))
@@ -23,14 +23,15 @@ def updateCanvas(C):
         width = 0
         if (row, column) in gol.universe:
             width = 1
-        tag = str(row) + ":" + str(column)
-        C.itemconfigure(tagOrId=str(row) + ":" + str(column), fill=color, width=width)
-
-
+#        tag = str(row) + ":" + str(column)
+        try:
+            C.itemconfigure(tagOrId=rect[(row,column)], fill=color, width=width)
+        except:
+            pass
 #
 # Interface routines for Tk
 #
-cellSize=7
+cellSize=15
 def toggle(e):
     """
     Calll back function for taggling cell, calculates screen coordinates in universum coordinate
@@ -74,7 +75,7 @@ def loopb():
     global gol
     gol.step()
     refreshScreen += 1
-    if refreshScreen % 7 == 0:  # Update screen only ever 10th step for speed up
+    if refreshScreen % 23 == 0:  # Update screen only ever 10th step for speed up
         updateCanvas(C)
     if (keepRunning):  # chekc whether stop button was pressedn
         top.after(1, loopb)
@@ -99,6 +100,24 @@ def clearb():
     updateCanvas(C)
     print("cleared...")
 
+def loadfile():
+    global C
+    global gol
+    with tkinter.filedialog.askopenfile(mode='r') as f:
+        s =f.read()
+ #   print (s)
+    (u,stay,get)= s.split(";",-1)
+    gol=GoL(eval(u),eval(stay),eval(get))
+    updateCanvas(C)
+
+
+def savefile():
+    with tkinter.filedialog.asksaveasfile(mode='w') as f:
+            f.write(str(gol.universe)+
+                    ";"+
+                    str(gol.stay_alive)+
+                    ";"+
+                    str(gol.get_born))
 
 def GoLGUI():
     """ Start the GUI interface for the Game of Life"""
@@ -106,43 +125,56 @@ def GoLGUI():
     global top  # make the top window available to the background tasks and to whoever needs it
     global refreshScreen  # make the generation counter acccessible to whoever needs it
     global loopButton
+    global rect
+    rect={}
     refreshScreen = 0
     top = tkinter.Tk()
     top.title("Game of Life")
     l = tkinter.Label(top, text="Game of Life")
     l.config(font=("Arial", 44))
     l.pack()
-    C = tkinter.Canvas(top, bg="white",width=1800,height=800)
-    rect = {}
+    C = tkinter.Canvas(top, bg="white",width=cellSize*80,height=cellSize*50)
+
     # prepare the GUI Window with all cells predrawn, to be configured later
-    for row in range(150):
-        for column in range(80):
-            rect[(row, column)] = C.create_oval(row * cellSize, column * cellSize, row * cellSize + cellSize-1, column * cellSize + cellSize-1,
+    for row in range(80):
+        for column in range(50):
+            rect [(row,column)]=C.create_rectangle(row * cellSize, column * cellSize, row * cellSize + cellSize-1, column * cellSize + cellSize-1,
                                                 tag=str(row) + ":" + str(column))
-            C.tag_bind(rect[(row, column)], "<ButtonPress-1>", toggle)
+    #       C.tag_bind(rect[(row, column)], "<ButtonPress-1>", toggle)
+    C.bind("<ButtonPress-1>", toggle)
     C.pack()
     #
     # Command buttons and labels for interaction
     #
     buttonframe = tkinter.Frame(top)
-    buttonframe.pack(expand=True)
+    buttonframe.pack(side=tkinter.LEFT,expand=True)
     nextButton = tkinter.Button(buttonframe, text='Next', width=15, command=stepb)
     nextButton.config(font=("Arial", 14))
     nextButton.pack(side=tkinter.LEFT)
     loopButton = tkinter.Button(buttonframe, text='Loop', width=15, command=loopbFirst)
     loopButton.pack(side=tkinter.LEFT)
-   # stopButton = tkinter.Button(buttonframe, text='Stop', width=15, command=stopb)
+    clearButton = tkinter.Button(buttonframe, text="Clear", width=15, command=clearb)
+    clearButton.pack(side=tkinter.LEFT)
+
+    # stopButton = tkinter.Button(buttonframe, text='Stop', width=15, command=stopb)
    # stopButton.pack(side=tkinter.LEFT)
     global refreshLabel
     refreshLabel = tkinter.Label(buttonframe, text=str(refreshScreen), width=25)
     refreshLabel.pack(side=tkinter.LEFT)
     global CellCounterLabel
+
     CellCounterLabel = tkinter.Label(buttonframe, text=str(len(gol.universe)), width=25)
     CellCounterLabel.pack(side=tkinter.LEFT)
 
-    clearButton = tkinter.Button(buttonframe, text="Clear", width=15, command=clearb)
-    clearButton.pack(side=tkinter.LEFT)
+    menubar = tkinter.Menu(top)
+    filemenu = tkinter.Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Open", command=loadfile)
+    filemenu.add_command(label="Save", command=savefile)
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command=top.quit)
+    menubar.add_cascade(label="File", menu=filemenu)
 
+    top.config(menu=menubar)
     updateCanvas(C)
     top.mainloop()
 
