0b91a7991e6db933b9b8eef70910f32f7d5e6816
Papa <qualcunodue@gmail.com>
2021-11-17 10:26:41 +0100

    Initial version

---
+++ GoLOO.py
@@ -0,0 +1,60 @@
+"""
+base class for Game of life
+"""
+
+class GoL():
+    """ Class containing one instance of a game of life board
+Methods
+ init  initialise game and rules
+    """
+
+    def __init__(self, cell_list={}, stay_alive={2, 3}, get_born={3}):
+        """
+        create initialise board, , and rules for when a cell is to be born
+        or survive depending on the number of its neighbours
+        """
+        self.universe = set(cell_list)
+        self.neighbours = {}
+        self.stay_alive = stay_alive
+        self.get_born = get_born
+
+    def toggle(self, x, y):
+        """ toggle a cell with row x and column y
+        """
+        if (x, y) in self.universe:
+            self.universe = self.universe - {(x, y)}
+            return False
+        else:
+            self.universe = self.universe | {(x, y)}
+            return True
+
+    def countNeighbours(self):
+        """ for each cell in universe count up the neighbors"""
+        self.neighbours = {}  # a sparse dictionary
+        for cell in self.universe:  # only for cells set in universe
+            (row, col) = cell
+            for rR in range(row - 1, row + 2):
+                for rC in range(col - 1, col + 2):  # 9 field square surrounding the cell
+                    try:
+                        self.neighbours[(rR, rC)] += 1
+                    except:
+                        self.neighbours[(rR, rC)] = 1
+            self.neighbours[cell] -= 1  # you are not your own neighbour
+        return self.neighbours
+
+    def step(self):
+        """
+        generate the next generation of cells
+        """
+        new_universe = set([])  # new universe initially empty
+        neighbours = self.countNeighbours()
+        print(neighbours)
+        for cell in neighbours:
+            if cell in self.universe:
+                if neighbours[cell] in self.stay_alive:
+                    new_universe = new_universe | {cell}
+                    print(str(cell) + "survived")
+            elif cell in self.get_born:
+                new_universe = new_universe | {cell}
+                print(str(cell) + "born")
+        self.universe = new_universe
---
+++ main.py
@@ -0,0 +1,160 @@
+import sys
+
+sys.path.append(".")
+from GoLOO import GoL
+import tkinter
+
+gol = GoL()
+
+colors = ["#FFFFFF", "#FFFF80", "#FFFF00", "#FF8000", "#FF4000", "#FF2000", "#FF0000", "#c00000", "#800000", "#000000"]
+
+
+def updateCanvas(C):
+    global rect
+    global refreshLabel
+    global refreshScreen
+    global CellCounterLabel
+    gol.countNeighbours()
+    refreshLabel.configure(text="Gernerations: " + str(refreshScreen))
+    CellCounterLabel.configure(text="Living Cells:  " + str(len(gol.universe)))
+    C.itemconfigure(tagOrId="all", fill="lightblue", width=0)
+    for (row, column) in gol.neighbours:
+        color = "white"
+        if (row, column) in gol.neighbours:
+            color = colors[gol.neighbours[(row, column)]]
+        width = 1
+        if (row, column) in gol.universe:
+            width = 5
+        tag = str(row) + ":" + str(column)
+        C.itemconfigure(tagOrId=str(row) + ":" + str(column), fill=color, width=width)
+
+
+#
+# Interface routines for Tk
+#
+
+def toggle(e):
+    """
+    Calll back function for taggling cell, calculates screen coordinates in universum coordinate
+    :param e: klick point
+    :return:
+    """
+    col = e.y // 10
+    row = e.x // 10
+    gol.toggle(row, col)
+    updateCanvas(C)
+
+
+def step(e):
+    """
+     Calll back function for taggling cell, calculates screen coordinates in universum coordinate
+     :param e: klick point
+     :return:
+     """
+    global gol
+    for i in range(1):
+        gol.step()
+    #    print(e)
+    updateCanvas(C)
+
+
+def stepb():
+    """
+     Button function for a single step ,
+     """
+    gol.step()
+    updateCanvas(C)
+
+
+refreshScreen = 0
+
+
+def loopbFirst():
+    """
+    start single stepping in background for the first time,
+    switch into keepRunning mode and lauch timer step generation
+    """
+    global keepRunning
+    keepRunning = True
+    loopb()
+
+
+def loopb():
+    """ background tasks called every millisecond """
+    global refreshScreen
+    global top
+    global keepRunning
+    stepGoL()
+    refreshScreen += 1
+    if refreshScreen % 1 == 0:  # Update screen only ever 10th step for speed up
+        updateCanvas(C)
+    if (keepRunning):  # chekc whether stop button was pressedn
+        top.after(1, loopb)
+
+
+def stopb():
+    """ switch of background task
+    """
+    global keepRunning
+    keepRunning = False
+
+
+def clearb():
+    """
+    Clear the universe and update display
+    """
+    global C
+    global gol
+    gol = GoL()
+    gol.countNeighbours()
+    updateCanvas(C)
+    print("cleared...")
+
+
+def GoLGUI():
+    """ Start the GUI interface for the Game of Life"""
+    global C  # make the canvas widget accessible to the updateCanvas function
+    global top  # make the top window available to the background tasks and to whoever needs it
+    global refreshScreen  # make the generation counter acccessible to whoever needs it
+    refreshScreen = 0
+    top = tkinter.Tk()
+    top.title("Game of Life")
+    l = tkinter.Label(top, text="Game of Life")
+    l.config(font=("Arial", 44))
+    l.pack()
+    C = tkinter.Canvas(top, bg="white", width=1000, height=800)
+    rect = {}
+    # prepare the GUI Window with all cells predrawn, to be configured later
+    for row in range(80):
+        for column in range(80):
+            rect[(row, column)] = C.create_oval(row * 10, column * 10, row * 10 + 9, column * 10 + 9,
+                                                tag=str(row) + ":" + str(column))
+            C.tag_bind(rect[(row, column)], "<ButtonPress-1>", toggle)
+    C.pack()
+    #
+    # Command buttons and labels for interaction
+    #
+    buttonframe = tkinter.Frame(top)
+    buttonframe.pack(expand=True)
+    nextButton = tkinter.Button(buttonframe, text='Next', width=15, command=stepb)
+    nextButton.config(font=("Arial", 14))
+    nextButton.pack(side=tkinter.LEFT)
+    loopButton = tkinter.Button(buttonframe, text='Loop', width=15, command=loopbFirst)
+    loopButton.pack(side=tkinter.LEFT)
+    stopButton = tkinter.Button(buttonframe, text='Stop', width=15, command=stopb)
+    stopButton.pack(side=tkinter.LEFT)
+    global refreshLabel
+    refreshLabel = tkinter.Label(buttonframe, text=str(refreshScreen), width=25)
+    refreshLabel.pack(side=tkinter.LEFT)
+    global CellCounterLabel
+    CellCounterLabel = tkinter.Label(buttonframe, text=str(len(gol.universe)), width=25)
+    CellCounterLabel.pack(side=tkinter.LEFT)
+
+    clearButton = tkinter.Button(buttonframe, text="Clear", width=15, command=clearb)
+    clearButton.pack(side=tkinter.LEFT)
+
+    updateCanvas(C)
+    top.mainloop()
+
+
+GoLGUI()
